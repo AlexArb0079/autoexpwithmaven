c8d7a84d4d251825eaf22003c647078c41710351
alexis.colman@arbusta.net <53272936+AlexArb0079@users.noreply.github.com>
2020-01-15 14:06:05 -0300

    init

---
+++ src/test/java/test/JavaTest.java
@@ -0,0 +1,24 @@
+package test;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class JavaTest {
+	
+	@Test
+	public void search() {
+		System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+		WebDriver driver;
+		driver = new ChromeDriver();
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
